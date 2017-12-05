c6b1ca9204bebd252c2b05800f9e8ab88c1aae8b
matthias toberer <matthias@toberer.net>
2017-12-05 01:04:15 +0100

    Add Permissions to ProfileView

--- src/profile_project/profile_api/views.py
+++ src/profile_project/profile_api/views.py
@@ -1,9 +1,9 @@
-from rest_framework import viewsets
+from django.shortcuts import render
+from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from . import serializers
-from rest_framework import status
-from . import models
+from rest_framework.authentication import TokenAuthentication
+from . import serializers, models, permissions
 
 class HelloApiView(APIView):
     """Test API View."""
@@ -95,6 +95,9 @@ class HelloViewSet(viewsets.ViewSet):
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """ Handles, creating and updateing profiles """
+    """Handles creating, creating and updating profiles."""
+
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
