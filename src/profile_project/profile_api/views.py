a3e21815ebcac733ca4d1d1613303604768a575d
matthias toberer <matthias@toberer.net>
2017-12-05 20:10:44 +0100

    Adding vs code workspace

--- src/profile_project/profile_api/views.py
+++ src/profile_project/profile_api/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import filters, status, viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import (IsAuthenticated, IsAuthenticatedOrReadOnly)
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from . import serializers
-from . import models
-from . import permissions
+from . import models, permissions, serializers
 
 # Create your views here.
 
